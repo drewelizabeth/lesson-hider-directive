a6e1f599f54577fab0f7c53956b65f5e34ee039a
Drew Larson <drewelizabeth@Drews-MacBook-Air.local>
2017-04-26 14:56:36 -0600

    cleared out master

--- js/lessonHider.js
+++
@@ -1,9 +0,0 @@
-angular.module('directivePractice').directive('lessonHider', function() {
-    return {
-        templateUrl: 'lessonHider.html',
-        restrict: 'E',
-        link: function(scope, element, attributes) {
-            console.log(scope, element, attributes);
-        }
-    }
-})
--- js/lessonService.js
+++
@@ -1,3 +0,0 @@
-angular.module('directivePractice').service('lessonService', function() {
-    
-})
